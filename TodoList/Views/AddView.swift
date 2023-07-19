8678812785dcafe557bd9552a457a8d1904ca7b9
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-19 17:39:57 +0300

    Add a ViewModel with @EnvironmentObject

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listVieweModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -20,9 +25,7 @@ struct AddView: View {
                     .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
                     .cornerRadius(10)
                 
-                Button(action: {
-                    
-                }, label: {
+                Button(action: saveButtonPressed, label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .font(.headline)
@@ -35,11 +38,35 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖊️")
+        .alert(isPresented: $showAlert, content: getAlert)
     }
     
-    struct AddView_Previews: PreviewProvider {
-        static var previews: some View {
-            AddView()
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listVieweModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
         }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item must be at least 3 characters long!!!🤨🧐🫠"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+        ListView()
+    }
+    .environmentObject(ListViewModel())
     }
 }
